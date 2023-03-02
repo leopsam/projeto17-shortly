7402632b8d2b4bfbcad3bcbab1b45deac31016ba
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-02 13:36:44 -0300

    folder architecture
    Criando arquitetura de todas as pastas e arquivos padrões do back-end

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use([])
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+});
