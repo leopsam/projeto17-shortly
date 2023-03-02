9960dccfbb21f0b03513c828f73c35d41ae3e49b
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-02 15:47:16 -0300

    update dump

--- dump.sql
+++ dump.sql
@@ -16,6 +16,289 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: rank; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.rank (
+    id integer NOT NULL,
+    "userShortId" integer NOT NULL,
+    "linksCount" integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: rank_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.rank_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: rank_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.rank_id_seq OWNED BY public.rank.id;
+
+
+--
+-- Name: short; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.short (
+    id integer NOT NULL,
+    url text NOT NULL,
+    "shortUrl" text NOT NULL,
+    "visitCount" integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: short_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.short_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: short_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.short_id_seq OWNED BY public.short.id;
+
+
+--
+-- Name: userShort; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."userShort" (
+    id integer NOT NULL,
+    "userId" integer NOT NULL,
+    "shortId" integer NOT NULL
+);
+
+
+--
+-- Name: userShort_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."userShort_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: userShort_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."userShort_id_seq" OWNED BY public."userShort".id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(18) NOT NULL,
+    "visitCount" integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: rank id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.rank ALTER COLUMN id SET DEFAULT nextval('public.rank_id_seq'::regclass);
+
+
+--
+-- Name: short id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.short ALTER COLUMN id SET DEFAULT nextval('public.short_id_seq'::regclass);
+
+
+--
+-- Name: userShort id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."userShort" ALTER COLUMN id SET DEFAULT nextval('public."userShort_id_seq"'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: rank; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: short; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: userShort; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Name: rank_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.rank_id_seq', 1, false);
+
+
+--
+-- Name: short_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.short_id_seq', 1, false);
+
+
+--
+-- Name: userShort_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."userShort_id_seq"', 1, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: rank rank_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.rank
+    ADD CONSTRAINT rank_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: short short_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.short
+    ADD CONSTRAINT short_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: userShort userShort_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."userShort"
+    ADD CONSTRAINT "userShort_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_name_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_name_key UNIQUE (name);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rank rank_userShortId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.rank
+    ADD CONSTRAINT "rank_userShortId_fkey" FOREIGN KEY ("userShortId") REFERENCES public."userShort"(id);
+
+
+--
+-- Name: userShort userShort_shortId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."userShort"
+    ADD CONSTRAINT "userShort_shortId_fkey" FOREIGN KEY ("shortId") REFERENCES public.short(id);
+
+
+--
+-- Name: userShort userShort_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."userShort"
+    ADD CONSTRAINT "userShort_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
 --
 -- PostgreSQL database dump complete
 --
