71e58a2628d0052791f3905302a7def6acd7cd61
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-02 20:14:55 -0300

    POST/ signin
    criando rota de post signin

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -21,3 +21,23 @@ export async function createUser(req, res) {
     res.send(error.message).status(422)
   }
 }
+
+export async function userLogin(req, res) {
+  console.log("rodou userLogin (login de usuario)") //deletar linha depois
+  const { email, password} = req.body  
+
+  const userExist = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])  
+  if(!userExist.rows[0]) return res.status(401).send("usuario ou senha invalidos")
+
+  try {    
+    if(bcrypt.compareSync(password, userExist.rows[0].password)){
+      const token = uuidV4()
+
+      res.status(200).send(token)  
+    }else{
+      res.status(401).send("usuario ou senha invalidos")
+    }
+  } catch (error) {
+    res.status(422).send(error.message)
+  }
+}
--- src/routers/users.routers.js
+++ src/routers/users.routers.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { createUser } from "../controllers/users.controllers.js"
+import { createUser, userLogin } from "../controllers/users.controllers.js"
 import { validateSchema } from "../middlewares/validateSchema.middleware.js"
 import { signupSchema } from "../schemas/signup.model.js"
+import { signinSchema } from "../schemas/signin.model.js"
 
 const router = Router()
 
 router.post("/signup", validateSchema(signupSchema), createUser)
+router.post("/signin", validateSchema(signinSchema), userLogin)
 
 export default router
