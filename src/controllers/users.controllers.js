65fd924e27fd31f40ca3802b4a83f9b43e398a9e
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-02 19:25:35 -0300

    Update users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -4,8 +4,15 @@ import { v4 as uuidV4 } from 'uuid'
 
 export async function createUser(req, res) {
   console.log("rodou createUser (criação de usuario)") //deletar linha depois
-  const { name, email, password} = req.body
+  const { name, email, password} = req.body  
   const passwordHash = bcrypt.hashSync(password, 10);
+
+  const userExist = await db.query(`SELECT * FROM users`)
+  userExist.rows.map((u)=>{
+    if(u.email === email){
+      return res.sendStatus(409)
+    }
+  })
 
   try {
     await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, passwordHash])
