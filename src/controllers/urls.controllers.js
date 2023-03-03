b68943933ff2c4e64a4ed14a18ce9f7e52bde286
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-03-02 21:38:08 -0300

    POST /urls/shorten

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,7 +1,22 @@
 import { db } from '../database/database.connection.js'
-import bcrypt from 'bcrypt'
-import { v4 as uuidV4 } from 'uuid'
+import { nanoid } from 'nanoid'
 
 export async function shorten(req, res) {
   console.log("rodou shorten (criando links curtos)") //deletar linha depois
+
+  const { url } = req.body 
+  const token = res.locals.token //erro
+  const shortUrl = nanoid()
+  console.log(shortUrl)
+
+  if (!token) return res.status(401).send("Você não está logado!") //erro
+
+  try {
+    await db.query(`INSERT INTO short (url, "shortUrl") VALUES ($1, $2);`, [url, shortUrl])
+
+    const short = await db.query(`SELECT * FROM short WHERE "shortUrl" = $1;`, [shortUrl])
+    res.status(201).send(short.rows)    
+  } catch (error) {
+    res.status(422).send(error.message)
+  }
 }
